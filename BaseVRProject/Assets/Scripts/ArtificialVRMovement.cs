ac833aea85f39114145e6e2020c78aa857c40c8f
Maximilian Maroe <drakfyre@gmail.com>
2020-06-04 12:24:30 -0700

    WIP new input system; getting some oddball results

--- BaseVRProject/Assets/Scripts/ArtificialVRMovement.cs
+++ BaseVRProject/Assets/Scripts/ArtificialVRMovement.cs
@@ -1,32 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ArtificialVRMovement : MonoBehaviour
 {
     public float maxSpeed = 1.0f;
-    public string horizontalAxisName = "Horizontal";
-    public string verticalAxisName = "Vertical";
+    public InputAction moveAction = new InputAction("moveAction", binding: "<LeftHand XR Controller>/thumbstick");
+
+    public enum MovementType {Head,LeftHand,RightHand}
+    public MovementType movementType;
     public bool threeDMovement = false;
 
     [Header("Required Scene/Child References")]
-    public Transform offhandMovementHand;
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
 
     [Header("Debug settings")]
     public bool logErrorsAfterInitialFailure = false;
 
-    private bool hasThrownError;
+    private bool hasThrownError = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (!offhandMovementHand)
+        if (!head || !leftHand || !rightHand)
         {
             LogError();
             return;
         }
 
-        Vector3 verticalMovementVector = offhandMovementHand.forward;
+        Vector3 verticalMovementVector;
+
+        switch (movementType)
+        {
+            case MovementType.LeftHand:
+                verticalMovementVector = leftHand.forward;
+                break;
+            case MovementType.RightHand:
+                verticalMovementVector = rightHand.forward;
+                break;
+            // Using default case to set head so verticalMovementVector counts as assigned
+            default:
+                verticalMovementVector = head.forward;
+                break;
+        }
 
         if (!threeDMovement)
         {
@@ -35,17 +54,33 @@ public class ArtificialVRMovement : MonoBehaviour
             verticalMovementVector = Vector3.ProjectOnPlane(verticalMovementVector, Vector3.up).normalized;
         }
 
-        Vector3 horizontalMovementVector = Vector3.Cross(verticalMovementVector,-Vector3.up);
+        Vector3 horizontalMovementVector = Vector3.Cross(verticalMovementVector, -Vector3.up);
 
-        transform.position += verticalMovementVector * Input.GetAxis(verticalAxisName) * Time.deltaTime;
-        transform.position += horizontalMovementVector * Input.GetAxis(horizontalAxisName) * Time.deltaTime;
+        Vector2 thumbstickMovementVector = moveAction.ReadValue<Vector2>();
+
+        Debug.Log(thumbstickMovementVector);  // WHY IS THIS ALWAYS 0.0f, 0.0f ?!?
+
+        transform.position += verticalMovementVector * thumbstickMovementVector.y * Time.deltaTime;
+        transform.position += horizontalMovementVector * thumbstickMovementVector.x * Time.deltaTime;
+    }
+
+    void OnEnable()
+    {
+        // Enable InputActions so they actually work
+        moveAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        // Disable InputActions just-in-case
+        moveAction.Disable();
     }
 
     void LogError()
     {
         if(logErrorsAfterInitialFailure || !hasThrownError)
         {
-            Debug.LogError("offhandMovementHand reference not set!", gameObject);
+            Debug.LogError("Not all Required Scene/Child References are set!", gameObject);
             hasThrownError = true;
         }
         return;
--- BaseVRProject/Assets/Scripts/WorldGrabMovement.cs
+++ BaseVRProject/Assets/Scripts/WorldGrabMovement.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 
 public class WorldGrabMovement : MonoBehaviour
 {
-    public HandInfo leftHand;
-    public HandInfo rightHand;
+    [Header("Don't forget to set Rigidbodies under Left/Right hand!")]
+
+    public HandInfo leftHand = new HandInfo(new InputAction("LeftGrip", binding: "<LeftHand XR Controller>/grip"));
+    public HandInfo rightHand = new HandInfo(new InputAction("RightGrip", binding: "<RightHand XR Controller>/grip"));
     public LayerMask grabbableObjectLayerMask = -1;
 
     [System.Serializable]
     public class HandInfo
     {
-        [Header("Required Object References")]
+        [Header("Required Component References")]
         public Rigidbody rigidbody;
-        public string gripAxis;
+        public InputAction gripAction;
 
         [Header("Debug Info")]
         public HandState state;
@@ -22,6 +25,11 @@ public class WorldGrabMovement : MonoBehaviour
         public Rigidbody grippedObject;
         public Vector3 previousPostion;
         public bool skip;
+
+        public HandInfo(InputAction newGripAction)
+        {
+            gripAction = newGripAction;
+        }
     }
     public enum HandState { Empty, MovementGrab, ObjectGrab }
 
@@ -37,11 +45,11 @@ public class WorldGrabMovement : MonoBehaviour
         WorldMovementLogic(leftHand,rightHand);
         WorldMovementLogic(rightHand,leftHand);
 
-        if(leftHand.skip && Input.GetAxis(leftHand.gripAxis) < 0.25f)
+        if(leftHand.skip && leftHand.gripAction.ReadValue<float>() < 0.25f)
         {
             leftHand.skip = false;
         }
-        if(rightHand.skip && Input.GetAxis(rightHand.gripAxis) < 0.25f)
+        if(rightHand.skip && rightHand.gripAction.ReadValue<float>() < 0.25f)
         {
             rightHand.skip = false;
         }
@@ -53,7 +61,7 @@ public class WorldGrabMovement : MonoBehaviour
     void ObjectGrabbingLogic(HandInfo hand)
     {
         Collider[] colliders = Physics.OverlapSphere(hand.previousPostion, 0.05f*transform.localScale.x, grabbableObjectLayerMask);
-        if (Input.GetAxis(hand.gripAxis) >= 0.25f)
+        if (hand.gripAction.ReadValue<float>() >= 0.25f)
         {
             if (colliders.Length > 0 && hand.state == HandState.Empty)
             {
@@ -73,7 +81,7 @@ public class WorldGrabMovement : MonoBehaviour
                 }
             }
         }
-        if (hand.state == HandState.ObjectGrab && Input.GetAxis(hand.gripAxis) < 0.25f)
+        if (hand.state == HandState.ObjectGrab && hand.gripAction.ReadValue<float>() < 0.25f)
         {
             Rigidbody rb = hand.grippedObject;
             FixedJoint joint = rb.GetComponent<FixedJoint>();
@@ -91,26 +99,40 @@ public class WorldGrabMovement : MonoBehaviour
 
     void WorldMovementLogic(HandInfo hand, HandInfo oppositeHand)
     {
-        if(!hand.skip && hand.state != HandState.ObjectGrab)
+        if (!hand.skip && hand.state != HandState.ObjectGrab)
         {
-            if (Input.GetAxis(hand.gripAxis) >= 0.25f)
+            if (hand.gripAction.ReadValue<float>() >= 0.25f)
             {
                 rigidbody.velocity = Vector3.zero;
                 transform.position += hand.previousPostion - hand.rigidbody.transform.position;
                 hand.state = HandState.MovementGrab;
 
-                if(oppositeHand.state == HandState.MovementGrab)
+                if (oppositeHand.state == HandState.MovementGrab)
                 {
                     oppositeHand.skip = true;
                     oppositeHand.state = HandState.Empty;
                 }
             }
-            if (hand.state == HandState.MovementGrab && Input.GetAxis(hand.gripAxis) < 0.25f)
+            if (hand.state == HandState.MovementGrab && hand.gripAction.ReadValue<float>() < 0.25f)
             {
                 rigidbody.velocity = (hand.previousPostion - hand.rigidbody.transform.position) / Time.deltaTime;
                 hand.state = HandState.Empty;
             }
         }
+    }
+
+    void OnEnable()
+    {
+        // Enable InputActions so they actually work
+        leftHand.gripAction.Enable();
+        rightHand.gripAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        // Disable InputActions just-in-case
+        leftHand.gripAction.Disable();
+        rightHand.gripAction.Disable();
     }
 
     void OnValidate()
