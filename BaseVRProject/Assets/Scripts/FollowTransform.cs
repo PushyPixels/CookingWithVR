57b7cf2fb67302baf421f5e12e89dce73868228d
Maximilian Maroe <drakfyre@gmail.com>
2020-07-28 15:24:55 -0700

    Basic roomscale collision; you can now physically walk off edges (And fall in general)

---
+++ BaseVRProject/Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform target;
+    public bool followX = true;
+    public bool followY = true;
+    public bool followZ = true;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = transform.position;
+
+        if(followX)
+        {
+            newPosition.x = target.position.x;
+        }
+
+        if(followY)
+        {
+            newPosition.y = target.position.y;
+        }
+
+        if(followZ)
+        {
+            newPosition.z = target.position.z;
+        }
+
+        transform.position = newPosition;
+    }
+}
